2f86e0d0648e3badbb3cc182641592d7e80899ec
Sanjib Subedi <sanjibsubedi2001@gmail.com>
2024-03-30 14:37:58 +0545

    Redirect from login and signup to home if user is logged in

--- src/app/(pages)/account/layout.tsx
+++ src/app/(pages)/account/layout.tsx
@@ -1,19 +1,21 @@
+"use client";
 import React from "react";
-import {
-    Card
-} from "@/components/ui/card"
+import { useAppSelector } from "@/redux/store";
+import { useRouter } from "next/navigation";
+import { Card } from "@/components/ui/card";
 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const isAuthenticated = useAppSelector((state) => state.authReducer.isAuthenticated);
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <section className="h-screen flex justify-center items-center">
-            <Card className="w-96 bg-primary-foreground">
-                {children}
-            </Card>
-        </section>
-    );
+  if (isAuthenticated) {
+    router.replace("/");
+    return null;
+  }
+
+  return (
+    <section className="h-screen flex justify-center items-center">
+      <Card className="w-96 bg-primary-foreground">{children}</Card>
+    </section>
+  );
 }
